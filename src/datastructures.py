fa22df27a357b3935c4d3839966fe283f5d655c8
Pau Romeu Bruguera <pau.romeu.bruguera@gmail.com>
2024-05-30 14:29:53 +0000

    first commit

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,6 +1,5 @@
-
 """
-update this file to implement the following already declared methods:
+Update this file to implement the following already declared methods:
 - add_member: Should add a member to the self._members list
 - delete_member: Should delete a member from the self._members list
 - update_member: Should update a member from the self._members list
@@ -8,29 +7,51 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
+        self._members = [{'id': 10,
+                          'first_name': 'John',
+                          'last_name': self.last_name,
+                          'age': 33,
+                          'lucky_numbers': [7, 13, 22]},
+                         {'id': self._generate_id(),
+                          'first_name': 'Jane',
+                          'last_name': self.last_name,
+                          'age': 35,
+                          'lucky_numbers': [10, 14, 3]},
+                         {'id': self._generate_id(),
+                          'first_name': 'Jimmy',
+                          'last_name': self.last_name,
+                          'age': 5,
+                          'lucky_numbers': [1]}]  # Example list of members
 
-        # example list of members
-        self._members = []
-
-    # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
+    def _generate_id(self):
+        # Read-only: Use this method to generate random members ID's when adding members into the list
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        # Fill this method and update the return
+        member['id'] = self._generate_id()
+        member['last_name'] = self.last_name()
+        self._members.append(member)
+        return self._members
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        # Fill this method and update the return
+        del self._members
+        return id
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        # Opcion 1 - for in
+        """  for row in self._members:
+            if row['id'] == id:
+                return row """
+        # Opcion 2 - List comprehension
+        result = [row for row in self._members if row['id'] == id]
+        return result[0] if result else None
 
-    # this method is done, it returns a list with all the family members
     def get_all_members(self):
+        # This method is done, it returns a list with all the family members
         return self._members
